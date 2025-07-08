f44f8cb7615596868d511d46f015c9b439301c8a
Anthony <actabelarmino@gmail.com>
2025-07-09 07:09:55 +0800

    initial

---
+++ ProjectWebAPI/CompanyEmployees/Program.cs
@@ -0,0 +1,28 @@
+using CompanyEmployees.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIIS();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
